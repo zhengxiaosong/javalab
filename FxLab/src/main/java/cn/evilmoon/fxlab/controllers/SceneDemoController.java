d872acf25bc0abb47b147b17b371aa90e24f9bbc
念 <xs.arthur@foxmail.com>
2018-11-15 21:17:43 +0800

    test

---
+++ FxLab/src/main/java/cn/evilmoon/fxlab/controllers/SceneDemoController.java
@@ -0,0 +1,19 @@
+package cn.evilmoon.fxlab.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.Pane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SceneDemoController implements Initializable {
+
+    @FXML
+    private Pane canvas;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
